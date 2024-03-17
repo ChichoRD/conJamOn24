eea1b50d96dae2baab0f67c76f824b0b2d813c91
Haoshuang <haohou@ucm.es>
2024-03-17 19:00:04 +0100

    Dialogo

--- Red Lines/Assets/Art/Scripts/DialogueController.cs
+++ Red Lines/Assets/Art/Scripts/DialogueController.cs
@@ -1,20 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueController : MonoBehaviour
 {
     [SerializeField, TextArea(4,6)] private string[] _dialogueLines;
-    [SerializeField] private GameObject 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _dialoguePanel;
+    [SerializeField] private TMP_Text _dialogueText;
+
+    [SerializeField] private float _time=0.05f;
+
+    private bool _didDialogueStart=false;
+    private int _lineIndex;
+
+    private void OnMouseDown()
     {
         
     }
+    void Start()
+    {
+        _dialoguePanel.SetActive(false);
+    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!_didDialogueStart)
+        {
+            StartDialogue();
+        }
+    }
+    private void StartDialogue()
+    {
+        _didDialogueStart = true;
+        _dialoguePanel.SetActive(true);
+        _lineIndex = 0;
+        StartCoroutine(ShowLine());
+    }
+    private IEnumerator ShowLine()
+    {
+        _dialogueText.text = string.Empty;
+
+        foreach(char c in _dialogueLines[_lineIndex])
+        {
+            _dialogueText.text += c;
+            yield return new WaitForSeconds(_time);
+        }
     }
 }
