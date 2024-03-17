04d477c2348f81590835d9024ace9bfc7670bd12
MaEduardaB <mbeckers@ucm.es>
2024-03-17 16:37:57 +0100

    Cosas del texto

--- Red Lines/Assets/Art/Scripts/DetectMouse.cs
+++ Red Lines/Assets/Art/Scripts/DetectMouse.cs
@@ -1,15 +1,36 @@
 using ReignCollectionSystem;
+using ReignSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class DetectMouse : MonoBehaviour
 {
     [SerializeField] private GameObject ventanaEmergente; // Referencia al GameObject de la ventana emergente
-    [SerializeField] private ReignLayoutType layoutType;
+    [SerializeField] private ReignLayoutType _layoutType;
+    [SerializeField] private ReignLayout _reignLayout;
+    private string _water;
+    private string _food;
+    private string _agenda;
+    private string _content;
+    [SerializeField] private TextMeshProUGUI _texto;
+
     void OnMouseEnter() {
         // Mostrar la ventana emergente cuando el ratón entra en el área del país
         ventanaEmergente.SetActive(true);
+        if (_reignLayout.TryGetReign(_layoutType, out Reign reign)) {
+            _water = reign.Parameter.water.ToString();
+            _food=reign.Parameter.food.ToString();
+            _agenda=reign.Parameter.agenda.ToString();
+            _content=reign.Parameter.content.ToString();
+            _texto.text = "Agua: " + _water + "\n" +
+                       "Comida: " + _food + "\n" +
+                        "Población: " + _water + "\n" +
+                        "Agenda: " + _agenda + "\n" +
+                        "Satisfacción: " + _content;
+        }
+        
     }
 
     void OnMouseExit() {
