3b12db84f72a1746210ea66f431c109e4c522a9e
Haoshuang <haohou@ucm.es>
2024-03-17 04:38:43 +0100

    MenuPausa

--- Red Lines/Assets/Art/Animation/UIAnimations/UIManager.cs
+++ Red Lines/Assets/Art/Animation/UIAnimations/UIManager.cs
@@ -1,7 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -11,10 +12,14 @@ public class UIManager : MonoBehaviour
     private Animator _transicion;
     [SerializeField]
     private Animator _buttonSkipAnimator;
+    [SerializeField]
+    private GameObject _pauseMenu;
     private bool _boolSkip = false;
     private bool _boolVentana = false;
+    private bool _pausado = false;
     void Start() {
         _noticia.SetActive(false);
+        _pauseMenu.SetActive(false);
     }
     
     public void AbrirCerrarVentana() {
@@ -45,9 +50,28 @@ public class UIManager : MonoBehaviour
         // Espera el tiempo especificado
         yield return new WaitForSeconds(tiempo);
 
-        // Desactiva la animaci�n
+        // Desactiva la animaci髇
         _transicion.SetBool("Activo", false);
         _boolSkip = false;
     }
-
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+        _pauseMenu.SetActive(true);
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+        _pauseMenu.SetActive(false);
+        _pausado = false;
+    }
+    public void Reiniciar()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
